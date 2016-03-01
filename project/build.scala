7d15fb0a2470be215329e631fe5fdfd98d59d7f7
Chris <chris@spendabit.co>
2016-03-01 15:33:01 -0500

    project/build.scala: Increment minor version num.

--- project/build.scala
+++ project/build.scala
@@ -4,7 +4,7 @@ import xerial.sbt.Sonatype.SonatypeKeys.sonatypeProfileName
 
 object build extends Build {
 
-  val libVersion = "0.0.52"
+  val libVersion = "0.0.53"
 
   lazy val project = Project (
     "webapp-tools",
