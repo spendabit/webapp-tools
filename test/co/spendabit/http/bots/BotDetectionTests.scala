7d0cf9d327a32b1551352d22708ccd0d51d0c834
Chris <chris@spendabit.co>
2016-03-28 22:33:39 -0400

    BotDetectionTests: Add additional test User-Agent string for testing bot-detection.

--- test/co/spendabit/http/bots/BotDetectionTests.scala
+++ test/co/spendabit/http/bots/BotDetectionTests.scala
@@ -7,7 +7,8 @@ class BotDetectionTests extends FunSuite {
   test("detecting various and sundry bot User-Agent strings") {
     val botUAs = Seq(
       "mfibot/1.1 (http://www.mfisoft.ru/analyst/; <admin@mfisoft.ru>; en-RU)",
-      "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)"
+      "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
+      "Wotbox/2.01 (+http://www.wotbox.com/bot/)"
     )
     botUAs.foreach { ua =>
       assert(looksLikeBotUserAgentString(ua).contains(true),
