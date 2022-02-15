7bbf2176279d009e498e66848bcdb4b15eb8dd80
Chris <chris@spendabit.co>
2022-02-15 11:38:07 -0500

    forms.v3.controls.Password: Use 'isEmpty' in place of 'length == 0'.

--- src/co/spendabit/webapp/forms/v3/controls/Password.scala
+++ src/co/spendabit/webapp/forms/v3/controls/Password.scala
@@ -9,7 +9,7 @@ case class Password(minLength: Int) extends GenericInput[String] {
   def validate(s: String): Either[String, String] =
     if (s.trim.length >= minLength)
       Right(s)
-    else if (s.trim.length == 0)
+    else if (s.trim.isEmpty)
       Left(s"Please provide a password.")
     else
       Left(s"The password must be at least $minLength characters long.")
